1626d225cb73b16ebfd3f7616f6365199bcbb399
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-03-13 12:51:29 +0300

    header, footer, auth pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
