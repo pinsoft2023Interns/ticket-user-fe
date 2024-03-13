a6469f1451b825ad91d8fb3652bb6eaeab8ab5b3
GoktugYmsk <gogtug033@outlook.com>
2024-03-13 12:23:10 +0300

    added typescript

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+import styles from "./page.module.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./page.module.css";
+
+const Home: React.FC = () => {
+  return (
+    <main className={styles.main}>
+      <div>Pinsoft</div>
+    </main>
+  );
+};
+
+export default Home;
