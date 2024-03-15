b699762b96134c7575aaa1c227f9b4431d04bf08
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-03-15 16:35:49 +0300

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Example() {
                   <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                     <Combobox.Input
                       className="w-full border-none py-4 pl-3 pr-10 text-lg leading-5 text-gray-900 focus:ring-0 h-full"
-                      displayValue={(city) => city.name}
+                      displayValue={(city: { name: string }) => city.name}
                       onChange={(event) => setQuery(event.target.value)}
                     />
                     <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -86,7 +86,7 @@ export default function Example() {
                     leaveTo="opacity-0"
                     afterLeave={() => setQuery("")}
                   >
-                    <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                    <Combobox.Options className="absolute mt-1 max-h-60 w-full z-50 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                       {filteredFrom.length === 0 && query !== "" ? (
                         <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                           Nothing found.
@@ -98,13 +98,13 @@ export default function Example() {
                             className={({ active }) =>
                               `relative cursor-default select-none py-2 pl-10 pr-4 ${
                                 active
-                                  ? "bg-teal-600 text-white"
+                                  ? "bg-indigo-600 text-white"
                                   : "text-gray-900"
                               }`
                             }
                             value={city}
                           >
-                            {({ selectedFrom, active }) => (
+                            {({ active }) => (
                               <>
                                 <span
                                   className={`block truncate ${
@@ -113,18 +113,6 @@ export default function Example() {
                                 >
                                   {city.name}
                                 </span>
-                                {selectedFrom ? (
-                                  <span
-                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                      active ? "text-white" : "text-teal-600"
-                                    }`}
-                                  >
-                                    <CheckIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                ) : null}
                               </>
                             )}
                           </Combobox.Option>
@@ -164,7 +152,7 @@ export default function Example() {
                   <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                     <Combobox.Input
                       className="w-full border-none py-4 pl-3 pr-10 text-lg leading-5 text-gray-900 focus:ring-0 h-full"
-                      displayValue={(city) => city.name}
+                      displayValue={(city: { name: string }) => city.name}
                       onChange={(event) => setQuery(event.target.value)}
                     />
                     <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -199,7 +187,7 @@ export default function Example() {
                             }
                             value={city}
                           >
-                            {({ selectedTo, active }) => (
+                            {({ active }) => (
                               <>
                                 <span
                                   className={`block truncate ${
