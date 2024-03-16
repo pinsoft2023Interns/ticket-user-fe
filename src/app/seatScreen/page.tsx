e036733166b07502fe58147ffeac2b03c677ee09
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-03-16 23:41:35 +0300

    bug fix

--- src/app/seatScreen/page.tsx
+++ src/app/seatScreen/page.tsx
@@ -1,27 +1,7 @@
 import React from "react";
 
 function SeatScreen(): JSX.Element {
-  const containerStyle: React.CSSProperties = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "600px",
-  };
-
-  const imageStyle: React.CSSProperties = {
-    transform: "rotate(-90deg) scale(0.3)",
-  };
-
-  return (
-    <div style={containerStyle}>
-      <img
-        src="https://venturebustours.com/images/28_LX_Chart.jpg"
-        style={imageStyle}
-        alt="Seat Map"
-        className="transform rotate-90 scale-50"
-      />
-    </div>
-  );
+  return <div className="flex items-center justify-center h-[600px]"></div>;
 }
 
 export default SeatScreen;
