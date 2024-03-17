86e83a27ea0edd999c16bf380b0020454b51ee3b
GoktugYmsk <gogtug033@outlook.com>
2024-03-17 18:18:43 +0300

    added intercepter

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -80,10 +80,10 @@ function Login() {
                 </a>
               </div>
               <button
-                type="submit"
-                className="w-full text-blue-500 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-full"
               >
-                Log in to your account
+                Giriş Yap
               </button>
               <p className="text-sm font-light text-center text-gray-500 dark:text-gray-300">
                 <a
