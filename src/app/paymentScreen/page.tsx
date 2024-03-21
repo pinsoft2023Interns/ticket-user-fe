1d00daa409fc0c5d68804f6086b5cf31ea68db0e
GoktugYmsk <gogtug033@outlook.com>
2024-03-21 23:01:44 +0300

    added paymnet screen

--- src/app/paymentScreen/page.tsx
+++ src/app/paymentScreen/page.tsx
@@ -1,14 +1,21 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function PaymentScreen() {
   const [cardNumber, setCardNumber] = useState("4256 4256 4256 4256");
   const [expDate, setExpDate] = useState("12/24");
-  const [ccv, setCcv] = useState("342");
+  const [ccv, setCcv] = useState("");
   const [cardName, setCardName] = useState("John Doe");
+  const [isValid, setIsValid] = useState(false);
+  const [isBackVisible, setIsBackVisible] = useState(false);
 
   const handleCardNumberChange = (event) => {
-    const input = event.target.value.replace(/\D/g, "");
+    let input = event.target.value.replace(/\D/g, "");
+
+    if (input.length > 16) {
+      input = input.slice(0, 16);
+    }
+
     let formattedInput = input.match(/.{1,4}/g);
     if (formattedInput) {
       formattedInput = formattedInput.join(" ");
@@ -26,97 +33,122 @@ function PaymentScreen() {
   };
 
   const handleCcvChange = (event) => {
-    const input = event.target.value.replace(/\D/g, "");
+    let input = event.target.value.replace(/\D/g, "");
+    if (input.length > 3) {
+      input = input.slice(0, 3);
+    }
     setCcv(input);
+    setIsBackVisible(input.length >= 1);
   };
 
   const handleCardNameChange = (event) => {
     setCardName(event.target.value);
   };
 
+  const handleSubmit = () => {
+    console.log("Payment successfully submitted");
+  };
+
+  useEffect(() => {
+    setIsValid(cardNumber.trim() !== "");
+  }, [cardNumber]);
+
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-6 lg:p-24">
-        <form className="bg-white w-full max-w-3xl mx-auto px-4 lg:px-6 py-8 shadow-md rounded-md flex flex-col lg:flex-row">
-          <div className="w-full lg:w-1/2 lg:pr-8 lg:border-r-2 lg:border-slate-300">
-            <div className="mb-4">
-              <label
-                htmlFor="cardNumber"
-                className="text-neutral-800 font-bold text-sm mb-2 block"
-              >
-                Card number:
-              </label>
-              <input
-                id="cardNumber"
-                type="text"
-                onChange={handleCardNumberChange}
-                className="flex h-10 w-full rounded-md border-2 bg-background px-4 py-1.5 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 undefined"
-                placeholder="XXXX XXXX XXXX XXXX"
-                value={cardNumber}
+      <div className="m-4">
+        <div className="credit-card w-1/3 shadow-lg mx-auto rounded-xl bg-white relative">
+          <div className="flex flex-col justify-center items-center">
+            <div className={`relative ${isBackVisible ? "rotate-180" : ""}`}>
+              <img
+                className="w-full h-auto"
+                src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
+                alt="front credit card"
               />
-            </div>
-            <div className="flex gap-x-2 mb-4">
-              <div className="block">
-                <label
-                  htmlFor="expDate"
-                  className="text-neutral-800 font-bold text-sm mb-2 block"
-                >
-                  Exp. date:
-                </label>
-                <input
-                  id="expDate"
-                  type="text"
-                  onChange={handleExpDateChange}
-                  className="flex h-10 w-full rounded-md border-2 bg-background px-4 py-1.5 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 undefined"
-                  placeholder="MM/YY"
-                  value={expDate}
-                />
+              <div className="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
+                <p className="number mb-5 sm:text-xl">
+                  {cardNumber !== "" ? cardNumber : "0000 0000 0000 0000"}
+                </p>
+                <div className="flex flex-row justify-between">
+                  <p>{cardName !== "" ? cardName : "Card holder"}</p>
+                  <div>
+                    <span>{expDate.split("/")[0]}</span>
+                    <span>/</span>
+                    <span>{expDate.split("/")[1]}</span>
+                  </div>
+                </div>
               </div>
-              <div className="block">
-                <label
-                  htmlFor="ccvNumber"
-                  className="text-neutral-800 font-bold text-sm mb-2 block"
-                >
-                  CCV:
-                </label>
-                <input
-                  id="ccvNumber"
-                  type="text"
-                  onChange={handleCcvChange}
-                  className="flex h-10 w-full rounded-md border-2 bg-background px-4 py-1.5 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 undefined"
-                  placeholder="XXX"
-                  value={ccv}
-                />
-              </div>
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="cardHolder"
-                className="text-neutral-800 font-bold text-sm mb-2 block"
+              <div
+                className={`back bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12 rotate-180 transition-transform ${
+                  isBackVisible ? "opacity-100" : "opacity-0"
+                } z-10`}
               >
-                Cardholder name:
-              </label>
-              <input
-                id="cardHolder"
-                type="text"
-                onChange={handleCardNameChange}
-                className="flex h-10 w-full rounded-md border-2 bg-background px-4 py-1.5 text-lg ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 undefined"
-                placeholder="John Doe"
-                value={cardName}
-              />
+                <div className="flex justify-end">
+                  <img
+                    className="w-full h-auto"
+                    src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-back.png"
+                    alt="back credit card"
+                  />
+                </div>
+                <div className="flex justify-end text-xs">
+                  <p className="flex z-30 text-3xl"> {ccv}</p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="w-full lg:w-1/2 lg:pl-8">
-            <div className="bg-slate-300 w-full h-44 rounded-md mb-4"></div>
+          <div className="mt-4 p-4">
+            <h1 className="text-xl font-semibold text-gray-700 text-center">
+              Card payment
+            </h1>
+            <div>
+              <div className="my-3">
+                <input
+                  type="text"
+                  className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                  placeholder="Card holder"
+                  value={cardName}
+                  onChange={handleCardNameChange}
+                />
+              </div>
+              <div className="my-3">
+                <input
+                  type="text"
+                  className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                  placeholder="Card number"
+                  value={cardNumber}
+                  onChange={handleCardNumberChange}
+                />
+              </div>
+              <div className="my-3 flex flex-col">
+                <div className="mb-2">
+                  <label htmlFor="" className="text-gray-700">
+                    Expired
+                  </label>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                  <input
+                    type="text"
+                    className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                    placeholder="Security code"
+                    value={ccv}
+                    onChange={handleCcvChange}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="mt-6 p-4">
             <button
-              type="submit"
-              className="w-full bg-primary text-white font-bold py-2 px-4 rounded-md hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+              className={`submit-button px-4 py-3 rounded-full bg-blue-300 text-blue-900 focus:ring focus:outline-none w-full text-xl font-semibold transition-colors ${
+                isValid ? "" : "opacity-50 cursor-not-allowed"
+              }`}
+              onClick={handleSubmit}
+              disabled={!isValid}
             >
-              Pay Now
+              Pay now
             </button>
-          </div>
-        </form>
-      </main>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
