3ae1438541b10d877f1da4619d4ac387feab3268
GoktugYmsk <gogtug033@outlook.com>
2024-03-23 11:21:05 +0300

    added visa card

---
+++ src/app/paymentScreen/master.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Master() {
+  return <div>Master</div>;
+}
+
+export default Master;
--- src/app/paymentScreen/page.tsx
+++ src/app/paymentScreen/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import VisaCard from "./visa";
 
 function PaymentScreen() {
   const [cardNumber, setCardNumber] = useState("4256 4256 4256 4256");
@@ -8,6 +9,9 @@ function PaymentScreen() {
   const [cardName, setCardName] = useState("John Doe");
   const [isValid, setIsValid] = useState(false);
   const [isBackVisible, setIsBackVisible] = useState(false);
+  const [cardImage, setCardImage] = useState(
+    "https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
+  );
 
   const handleCardNumberChange = (event) => {
     let input = event.target.value.replace(/\D/g, "");
@@ -21,6 +25,14 @@ function PaymentScreen() {
       formattedInput = formattedInput.join(" ");
     }
     setCardNumber(formattedInput || "");
+
+    if (input.charAt(0) === "4") {
+      setCardImage(
+        "https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
+      );
+    } else if (input.charAt(0) === "5") {
+      setCardImage("https://i.ibb.co/LPLv5MD/Payment-Card-01.jpg");
+    }
   };
 
   const handleExpDateChange = (event) => {
@@ -58,42 +70,14 @@ function PaymentScreen() {
       <div className="m-4">
         <div className="credit-card w-1/3 shadow-lg mx-auto rounded-xl bg-white relative">
           <div className="flex flex-col justify-center items-center">
-            <div className={`relative ${isBackVisible ? "rotate-180" : ""}`}>
-              <img
-                className="w-full h-auto"
-                src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
-                alt="front credit card"
-              />
-              <div className="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
-                <p className="number mb-5 sm:text-xl">
-                  {cardNumber !== "" ? cardNumber : "0000 0000 0000 0000"}
-                </p>
-                <div className="flex flex-row justify-between">
-                  <p>{cardName !== "" ? cardName : "Card holder"}</p>
-                  <div>
-                    <span>{expDate.split("/")[0]}</span>
-                    <span>/</span>
-                    <span>{expDate.split("/")[1]}</span>
-                  </div>
-                </div>
-              </div>
-              <div
-                className={`back bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12 rotate-180 transition-transform ${
-                  isBackVisible ? "opacity-100" : "opacity-0"
-                } z-10`}
-              >
-                <div className="flex justify-end">
-                  <img
-                    className="w-full h-auto"
-                    src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-back.png"
-                    alt="back credit card"
-                  />
-                </div>
-                <div className="flex justify-end text-xs">
-                  <p className="flex z-30 text-3xl"> {ccv}</p>
-                </div>
-              </div>
-            </div>
+            <VisaCard
+              isBackVisible={isBackVisible}
+              cardImage={cardImage}
+              cardNumber={cardNumber}
+              cardName={cardName}
+              expDate={expDate}
+              ccv={ccv}
+            />
           </div>
           <div className="mt-4 p-4">
             <h1 className="text-xl font-semibold text-gray-700 text-center">
