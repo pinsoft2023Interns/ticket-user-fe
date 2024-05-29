8aaa9e582ea23ca026860e94e41ed390b88087b3
Aylin Arık <arikayli1973@gmail.com>
2024-05-30 00:10:20 +0300

    bug fix

--- src/app/uyelik/seyahatlerim/page.tsx
+++ src/app/uyelik/seyahatlerim/page.tsx
@@ -1,59 +1,48 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 import "moment/locale/tr";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-type Ticket = {
+interface Ticket {
   id: number;
-  company: string;
-  date: string;
-  from: string;
-  fromPlatform: string;
-  to: string;
-  passenger: string;
-  pnr: string;
-  seat: string;
-  price: string;
-};
+  reservationTimestamp: string;
+  seatInfo: string;
+  price: number;
+  bnsIds: string;
+  pnrNumber: string;
+  departureDate: string;
+  name: string;
+}
 
 function MyTravels() {
-  const tickets: Ticket[] = [
-    {
-      id: 1,
-      company: "Isparta Petrol Turizm",
-      date: "28 03 2026 - 22:00",
-      from: "Denizli (Denizli Otogarı)",
-      fromPlatform: "(Peron 9)",
-      to: "Keşan (Keşan Otogarı)",
-      passenger: "GÖKTUĞ YUMUŞAK",
-      pnr: "1234567",
-      seat: "1",  
-      price: "650,00 TL",
-    },
-    {
-      id: 2,
-      company: "Balıkesir Uludağ Turizm",
-      date: "30 04 2023 - 18:00",
-      from: "İstanbul (Esenler Otogarı)",
-      fromPlatform: "(Peron 10)",
-      to: "Çanakkale (Çanakkale Otogarı)",
-      passenger: "Gökalp YUMUŞAK",
-      pnr: "8321242",
-      seat: "12",
-      price: "880,00 TL",
-    },
-    {
-      id: 3,
-      company: "Balıkesir Uludağ Turizm",
-      date: "20 05 2018 - 18:00",
-      from: "İstanbul (Esenler Otogarı)",
-      fromPlatform: "(Peron 10)",
-      to: "Çanakkale (Çanakkale Otogarı)",
-      passenger: "Cemal YUMUŞAK",
-      pnr: "8321222",
-      seat: "12",
-      price: "880,00 TL",
-    },
-  ];
+  const router = useRouter();
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://ticket-web-be-6ogu.onrender.com/busnavigation');
+        const data = response.data;
+
+        const ticketsWithDepartureDates = data.reduce((acc: Ticket[], busNav: any) => {
+          const departureDate = busNav.busNavStation.length > 0 ? busNav.busNavStation[0].departureDate : null;
+          const ticketsWithDates = busNav.tickets.map((ticket: Ticket) => ({
+            ...ticket,
+            departureDate,
+          }));
+          return [...acc, ...ticketsWithDates];
+        }, []);
+
+        setTickets(ticketsWithDepartureDates);
+      } catch (error) {
+        console.error('API isteği sırasında hata oluştu:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   moment.locale("tr");
   const now = moment();
@@ -61,13 +50,13 @@ function MyTravels() {
   return (
     <>
       {tickets.map((ticket) => {
-        const ticketDate = moment(ticket.date, "DD MM YYYY - HH:mm", "tr");
+        const ticketDate = moment(ticket.departureDate,);
         const formattedDate = ticketDate.format("DD MMMM YYYY dddd - HH:mm");
 
         const isPastDate = ticketDate.isBefore(now);
 
         return (
-          <div key={ticket.id} className="mx-auto p-4 mt-10 ">
+          <div key={ticket.id} className="mx-auto p-4 mt-10">
             {isPastDate ? (
               <h2>Geçmiş Tarihli Bilet</h2>
             ) : (
@@ -81,34 +70,34 @@ function MyTravels() {
               }`}
             >
               <div className="flex justify-between text-center">
-                <h1 className="text-2xl font-semibold">{ticket.company}</h1>
+              <h1 className="text-2xl font-semibold">{ticket.id}</h1>
                 <p className="text-sm text-gray-500">{formattedDate}</p>
                 <div className="flex gap-6">
-                  <p className="text-sm text-gray-500">{ticket.from}</p>
-                  <p className="text-sm text-gray-500">{ticket.fromPlatform}</p>
-                  <p className="text-sm text-gray-500">{ticket.to}</p>
+                  <p className="text-sm text-gray-500">{ticket.bnsIds}</p>
+                  <p className="text-sm text-gray-500">{ticket.pnrNumber}</p>
+                  <p className="text-sm text-gray-500">{ticket.bnsIds}</p>
                 </div>
               </div>
               <hr className="my-4" />
               <div className="flex text-center justify-between">
                 <div className="flex flex-col">
-                  <p className="text-lg font-semibold">{ticket.passenger}</p>
+                <p className="text-lg font-semibold">{ticket.id}</p>
                   <p className="text-sm text-gray-500">YOLCULARIMA KAYITLI</p>
                 </div>
                 <div>
                   <p className="text-sm text-gray-500">PNR NO: </p>
-                  <p className="text-sm text-gray-500">{ticket.pnr}</p>
+                  <p className="text-sm text-gray-500">{ticket.pnrNumber}</p>
                 </div>
                 <p className="text-sm text-gray-500">
-                  KOLTUK NO: {ticket.seat}
+                  KOLTUK NO: {ticket.seatInfo}
                 </p>
-                <p className="text-sm text-gray-500">TUTAR: {ticket.price}</p>
+                <p className="text-sm text-gray-500">TUTAR: {ticket.price} TL</p>
               </div>
               <hr className="my-4" />
               <div className="flex text-center ">
-                <p className="text-lg font-semibold">Toplam Tutar: {ticket.price}</p>
+                <p className="text-lg font-semibold">Toplam Tutar: {ticket.price} TL</p>
               </div>
-              <div className="flex text-center mt-4  justify-between ">
+              <div className="flex text-center mt-4 justify-between">
                 <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2">
                   SEFER DETAYLARI İÇİN TIKLAYIN
                 </button>
