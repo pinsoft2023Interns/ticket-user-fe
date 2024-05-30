ccc9969695dd5570999e8f88f7cfc169037a0748
GoktugYmsk <gogtug033@outlook.com>
2024-05-30 13:11:39 +0300

    bugs are fixed

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,14 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import counterReducer from "./slice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = () => {
   return configureStore({
-    reducer: { counterReducer },
+    reducer: persistedReducer,
   });
 };
 
-export type AppStore = ReturnType<typeof store>;
+export const persistor = persistStore(store());
 
+export type AppStore = ReturnType<typeof store>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
--- src/store/storeProvider.tsx
+++ src/store/storeProvider.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useRef } from "react";
 import { Provider } from "react-redux";
-import { store, AppStore } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor, AppStore } from "./store";
 
 export default function StoreProvider({
   children,
@@ -13,5 +14,11 @@ export default function StoreProvider({
     storeRef.current = store();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
