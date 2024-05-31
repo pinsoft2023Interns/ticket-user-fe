75ad3debc1cd9dce4599e5318bf0943218f633d4
Aylin Arık <arikayli1973@gmail.com>
2024-06-01 00:38:18 +0300

    bug fix

--- src/app/uyelik/rezervasyonlarim/page.tsx
+++ src/app/uyelik/rezervasyonlarim/page.tsx
@@ -31,7 +31,6 @@ function MyReservations() {
             </div>
           </div>
           <div className="flex gap-2">
-            <p>Bu bilgi ticketScreen sayfasından gelmektedir.</p>
             <button className="text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none lg:col-span-3 col-span-6">
               Açığa Al
             </button>
